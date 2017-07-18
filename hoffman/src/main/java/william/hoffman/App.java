496e2d515089b5f5fdd00713dbb1a44faee2f299
william <william@william-NV47H>
2017-07-18 19:36:32 +0800

    first

---
+++ hoffman/src/main/java/william/hoffman/App.java
@@ -0,0 +1,13 @@
+package william.hoffman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
